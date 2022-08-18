625178b828f8cd23770aac9be9e050f9428cb12f
nikichDmITRO <dmitracenkonikita5@gmail.com>
2022-08-18 08:58:41 +0300

    Тренировался в слепую с квериселектором

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+
+let clickButton=document.querySelector(".button").onclick=ris
+
+
+function ris(){
+    let znac=document.querySelector(".input").value
+    document.querySelector(".out").innerHTML=znac
+}
